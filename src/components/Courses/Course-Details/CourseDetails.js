bc0cc645f2ada4bf3bcea8e43e2928b69f5e6caf
Mshujon <mshujon@MKs-MacBook-Pro.local>
2023-10-28 14:07:33 +0200

    exportPdf done

--- src/components/Courses/Course-Details/CourseDetails.js
+++ src/components/Courses/Course-Details/CourseDetails.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
+import { useReactToPrint } from 'react-to-print';
 
 
 const CourseDetails = () => {
    const singleCourse = useLoaderData();
    const singleCourseDetails = singleCourse[0];
+
+   const myRef = useRef(null);
+   const createPDF = useReactToPrint({
+       content: () => myRef.current,
+   });
    
    console.log(singleCourseDetails);
     return (
@@ -17,10 +23,10 @@ const CourseDetails = () => {
             </section>
             <section className='courses-details  flex justify-center mx-16 my-16 p-5 leading-loose text-xl' >
                 <div className='text-center mx-auto w-3/6'>
-                     <button className='btn rounded bg-amber-300 px-6 py-3 hover:bg-amber-200'>Download Course as Pdf</button>
+                     <button onClick={createPDF} className='btn rounded bg-amber-300 px-6 py-3 hover:bg-amber-200'>Download Course as Pdf</button>
                      <h1 className='text-5xl font-bold text-center mt-10'>{singleCourseDetails.name}</h1>
                      <p className='mt-10 leading-normal'>{singleCourseDetails.short_description}</p>
-                     <div>
+                     <div ref={myRef}>
                         <div className='shadow-md w-auto h-86 '><img className='rounded mt-10' src={singleCourseDetails.image} alt="" /></div>
                         <p className='mt-10 leading-normal'>{singleCourseDetails.details}</p>
                         <div className='mt-5 flex justify-center items-center w-auto mx-auto'>
