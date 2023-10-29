ceaac25dd48aa25341dc58f09386e65fe33a0194
Mshujon <mshujon@MKs-MacBook-Pro.local>
2023-10-29 21:02:04 +0100

    env correction

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD7azLRoTCVRZL0BiDWUViLrFmk_MbkSd8",
-    authDomain: "linda-education.firebaseapp.com",
-    projectId: "linda-education",
-    storageBucket: "linda-education.appspot.com",
-    messagingSenderId: "856233788788",
-    appId: "1:856233788788:web:126e4e99116922606cd821",
-    measurementId: "G-ZFKTG3PXS0"
+    apiKey: process.env.REACT_APP_apiKey, 
+    authDomain: process.env.REACT_APP_authDomain, 
+    projectId: process.env.REACT_APP_projectId, 
+    storageBucket: process.env.REACT_APP_storageBucket, 
+    messagingSenderId: process.env.REACT_APP_messagingSenderId, 
+    appId: process.env.REACT_APP_appId, 
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
